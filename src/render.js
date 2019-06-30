208d8a572e993c8966a293dee78ce8a3e43c3f2d
masx200 <34191203+masx200@users.noreply.github.com>
2019-06-30 11:51:38 +0800

    ditto框架全面升级!

--- src/render.js
+++ src/render.js
@@ -1,34 +1,37 @@
+import "./ditto.css";
 import "./RegisteraServiceWorkerFile.js";
-import "./prefetchmd.js";
 import ditto from "./ditto.js";
-// import READMEmd from "../README.md";
-// import sidebarmd from "../sidebar.md";
-(() => {
-  $(window).one("load", () => {
-    const clipboard = new ClipboardJS(".btn");
+import "./prefetchmd";
+import "./hidewidthless500.css";
+import "./index.css";
+var doctitle = "es6tutorial";
+$(window).one("load", () => {
+  const clipboard = new ClipboardJS(".btn");
 
-    clipboard.on("success", function(e) {
-      if (!e.text) {
-        console.log("复制内容空");
-      } else {
-        //   console.info("Action:", e.action);
-        //   console.info("Text:", e.text);
-      }
+  clipboard.on("success", function(e) {
+    if (!e.text) {
+      console.log("复制内容空");
+    } else {
+      //   console.info("Action:", e.action);
+      //   console.info("Text:", e.text);
+    }
 
-      e.clearSelection();
-    });
+    e.clearSelection();
+  });
 
+  (() => {
+    document.title = doctitle + "-Lightweight Markdown Documentation System";
+    $("#title").text(doctitle);
     $("#my主体").css("padding-top", $("#my导航栏").height());
-
-    $(function() {
+    (function() {
       // essential settings
       (ditto.index = "README.md"),
-        //   (ditto.index = READMEmd),
         (ditto.sidebar_file = "sidebar.md"),
-        // (ditto.sidebar_file = sidebarmd),
         // optional settings if you want github search
-        (ditto.github_username = "masx200"); // <------- EDIT ME!!
-      ditto.github_repo = "es6tutorial"; // <------- EDIT ME!!
+        (ditto.github_username = "masx200");
+      // <------- EDIT ME!!
+      ditto.github_repo = "es6tutorial";
+      // <------- EDIT ME!!
       // ditto.highlight_code = false; // <------- EDIT ME!!
       ditto.highlight_code = true;
       // where the docs are actually stored on github - so you can edit
@@ -36,7 +39,7 @@ import ditto from "./ditto.js";
 
       // run
       ditto.run();
-    });
+    })();
     // $("#sidebar > ul").addClass("navbar-nav")
     function onhashchange() {
       scrollTo(0, 0);
@@ -47,8 +50,7 @@ import ditto from "./ditto.js";
       //   }
     }
     $(window).on("hashchange", onhashchange);
-    new ClipboardJS(".btn");
-  });
-
-  // if(location.hash===""  )  {location.hash="#README"}
-})();
+    // new ClipboardJS(".btn");
+    // if(location.hash===""  )  {location.hash="#README"}
+  })();
+});
