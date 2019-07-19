4e1ecd9dad67fb2723b30932a4cb80a07ec0c1db
masx200 <34191203+masx200@users.noreply.github.com>
2019-07-19 13:18:04 +0800

    大升级，修复bug

---
+++ precache-manifest.0f349e5bb85e6c84317a17472c737452.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "./bundle.runtime~main.f7d070d58660a2a66449.js"
+  },
+  {
+    "url": "./chunk.0.c05bc9e755aee3dbbf2b.js"
+  },
+  {
+    "url": "./chunk.1.1b620267ed38d0d77564.js"
+  },
+  {
+    "url": "./chunk.4.36ba2ba3dd8971dbbb35.js"
+  },
+  {
+    "url": "./chunk.5.655ee8c5cdf34fd7dee4.js"
+  },
+  {
+    "url": "./chunk.6.bfc909c91d54e9422c7a.js"
+  },
+  {
+    "url": "./chunk.main.79db5fc9a4562874d1d1.js"
+  },
+  {
+    "url": "./main.79db5fc9a4562874d1d1.css"
+  }
+]);
---
+++ src/config.js
@@ -0,0 +1,6 @@
+export default {
+  doctitle: "ECMAScript 6 入门",
+  subtitle: "Lightweight Markdown Documentation System",
+  github_username: "masx200",
+  github_repo: "es6tutorial"
+};
