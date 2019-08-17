515247fc398c5984f79dd3d99d8c9af9062a8517
masx200 <34191203+masx200@users.noreply.github.com>
2019-08-17 16:07:36 +0800

    大升级

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export default {
   doctitle: "ECMAScript 6 入门",
   subtitle: "Lightweight Markdown Documentation System",
-  github_username: "masx200",
-  github_repo: "es6tutorial"
+  index: "README.md",
+
+  summary: "SUMMARY.md"
 };
