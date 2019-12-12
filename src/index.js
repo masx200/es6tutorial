928a67fdb0a6afb39f9336fc230d0481ea231a47
masx200 <34191203+masx200@users.noreply.github.com>
2019-12-12 22:43:46 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "@masx200/webpack-react-vue-spa-awesome-config/registerserviceworker.js";
-import { config, mountinit } from "markdown-reader/src/mark-down-reader";
+import { config, mountinit } from "@masx200/markdown-reader/src/mark-down-reader";
 Object.assign(config, {
   github_username: "masx200",
   github_repo: "es6tutorial",
